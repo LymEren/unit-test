6cfff4f14ee72dd717e30db118e665e13ce980a3
Eyyub Eren <eyyub.erenn@gmail.com>
2024-12-13 23:28:09 +0300

    BasketBusinessRules file added.

---
+++ crm-project/src/main/java/com/myProject/crm_project/rules/BasketBusinessRules.java
@@ -0,0 +1,21 @@
+package com.myProject.crm_project.rules;
+
+
+import com.myProject.crm_project.entities.Basket;
+import com.myProject.crm_project.repositories.BasketRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class BasketBusinessRules {
+    private BasketRepository basketRepository;
+
+    public boolean productCannotBeAddedTwice(String productName) {
+        Optional<Basket> productCheck = basketRepository.findByItemNameIgnoreCase(productName);
+        return productCheck.isPresent();
+    }
+
+}
--- crm-project/src/main/java/com/myProject/crm_project/services/BasketServiceImpl.java
+++ crm-project/src/main/java/com/myProject/crm_project/services/BasketServiceImpl.java
@@ -2,22 +2,38 @@ package com.myProject.crm_project.services;
 
 import com.myProject.crm_project.entities.Basket;
 import com.myProject.crm_project.repositories.BasketRepository;
+import com.myProject.crm_project.rules.BasketBusinessRules;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
-public class BasketServiceImpl implements BasketService{
+public class BasketServiceImpl implements BasketService {
 
     private BasketRepository basketRepository;
+    private BasketBusinessRules basketBusinessRules;
 
     @Override
     public Basket addItem(Basket basket) {
 
-        basketRepository.findByItemNameIgnoreCase(basket.getItemName());
-        return basketRepository.save(basket);
+        boolean isExist = basketBusinessRules.productCannotBeAddedTwice(basket.getItemName());
+
+        if (isExist) {
+            Optional<Basket> existingBasket = basketRepository.findByItemNameIgnoreCase(basket.getItemName());
+            if (existingBasket.isPresent()) {
+                Basket existingProduct = existingBasket.get();
+                existingProduct.setQuantity(existingProduct.getQuantity() + 1);
+                return basketRepository.save(existingProduct);
+            } else {
+                basket.setQuantity(1);
+                return basketRepository.save(basket);
+
+            }
+        }
+        return basket;
     }
 
     @Override
